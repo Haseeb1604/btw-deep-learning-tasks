d7d840c62abc1438eaab2fbfb9239073298c1d04
Muhammad Haseeb <haseeb200khan@gmail.com>
2023-03-26 21:09:30 +0500

    Updating task 6

--- Task 6/dict.py
+++ Task 6/dict.py
@@ -2,6 +2,9 @@
 # Key Value Paired craated using curly braces {}
 # : are used to seperate key and values
 
+# Creating an empty dictionary
+my_dict = {}
+
 # For Example Fruits along quantity
 my_dict = {'apple': 2, 'banana': 3, 'orange': 1}
 
@@ -16,3 +19,7 @@ print(my_dict) # Output: {'apple': 2, 'banana': 3, 'orange': 1, 'pear': 4}
 my_dict = {'apple': 2, 'banana': 3, 'orange': 1, 'pear': 4}
 del my_dict['pear']
 print(my_dict) # Output : {'apple': 2, 'banana': 3, 'orange': 1}
+
+# Changing Items to a Dictionary
+my_dict['banana'] = 5
+print(my_dict) # Output : {'apple': 2, 'banana': 5, 'orange': 1}
